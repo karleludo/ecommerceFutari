0374512182286fd838b6007300616bd4e042d10f
Robert Carlo Tubig <carlotubig@gmail.com>
2021-02-19 12:52:51 +0900

    another changes

--- ecommerceFutari-main/src/App.js
+++ ecommerceFutari-main/src/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
         return(
             <div>
                 <h1>Hello Ecommerce Futari</h1>
+                <h2>Hello For the 2nd time</h2>
             </div>
         )
     }
