24d9c22c73962d30da427531067b65475d5d132d
Robert Carlo Tubig <carlotubig@gmail.com>
2021-02-19 12:37:42 +0900

    some changes

---
+++ ecommerceFutari-main/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <div>
+  <h1>Hello World</h1>
+  <p>
+    this is a paragraph
+  </p>
+</div>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
